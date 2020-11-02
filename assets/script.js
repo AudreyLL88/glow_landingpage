ffa44e64d1d5cbfd6fe58a99f008411cbebde08f
AudreyLL88 <a.lloancy@gmail.com>
2020-11-02 11:18:23 +0000

    class change

--- assets/script.js
+++ assets/script.js
@@ -3,17 +3,16 @@
 const start_btn_slasher = document.querySelector(".start-btn-slasher button");
 const start_btn_monster = document.querySelector(".start-btn-monster button");
 const start_btn_paranormal = document.querySelector(".start-btn-paranormal button");
-const info_box = document.querySelector(".info_box");
-const exit_btn = info_box.querySelector(".buttons .quit");
-const continue_btn = info_box.querySelector(".buttons .restart");
+const rule_box = document.querySelector(".rule_box");
+const exit_btn = rule_box.querySelector(".buttons .quit");
+const continue_btn = rule_box.querySelector(".buttons .restart");
 const quiz_box = document.querySelector(".quiz_box");
 const option_list = document.querySelector(".option_list");
 const timeCount = document.querySelector(".timer .timer_sec");
-const timeLine = document.querySelector("header .time_line");
 const timeOff = document.querySelector("header .time_text");
-const slasherWrap = document.querySelector(".post-wrap1");
-const monsterWrap = document.querySelector(".post-wrap3");
-const paranormalWrap = document.querySelector(".post-wrap2");
+const slasherWrap = document.querySelector(".card-wrap1");
+const monsterWrap = document.querySelector(".card-wrap3");
+const paranormalWrap = document.querySelector(".card-wrap2");
 const quiz_back = document.querySelector(".quiz_back");
 
 let activeCard = "None"
@@ -22,45 +21,43 @@ let activeCard = "None"
 start_btn_slasher.onclick =()=>{
     activeCard = "slasher";
     quiz_back.classList.add("activeBack");
-    info_box.classList.add("activeInfo");
+    rule_box.classList.add("activeInfo");
     slasherWrap.classList.add("noHover");
 }
 
 start_btn_monster.onclick =()=>{
     activeCard = "monster";
     quiz_back.classList.add("activeBack");
-    info_box.classList.add("activeInfo");
+    rule_box.classList.add("activeInfo");
     monsterWrap.classList.add("noHover");
 }
 
 start_btn_paranormal.onclick =()=>{
     activeCard = "paranormal";
     quiz_back.classList.add("activeBack");
-    info_box.classList.add("activeInfo");
+    rule_box.classList.add("activeInfo");
     paranormalWrap.classList.add("noHover");
 }
 
 //If Exit Quiz button clicked
 
 exit_btn.onclick =()=>{
-    info_box.classList.remove("activeInfo");
+    rule_box.classList.remove("activeInfo");
 }
 
 //If continue Quiz button clicked
 
 continue_btn.onclick =()=>{
-    info_box.classList.remove("activeInfo");
+    rule_box.classList.remove("activeInfo");
     quiz_box.classList.add("activeQuiz");
     showQuestions(0);
     queCounter(1);
     startTimer(15);
-    startTimerLine(0);
 }
 
 let que_count =  0;
 let que_numb = 1;
 let counter;
-let counterLine; 
 let timeValue = 15;
 let widthValue = 0;
 let userScore = 0;
@@ -97,8 +94,6 @@ next_btn.onclick =()=>{
     queCounter(que_numb);
     clearInterval(counter);
     startTimer(timeValue);
-    clearInterval(counterLine);
-    startTimerLine(widthValue);
     next_btn.style.display = "none";
    }else{
        console.log("Questions completed");
@@ -121,13 +116,13 @@ function showQuestions(index){
         questionsArray = questions1;
     }
     const que_title = document.querySelector(".que_title");
-    const que_text = document.querySelector(".que_text");
+    const que_name = document.querySelector(".que_name");
     let que_tag = '<span>'+ questionsArray[index].numb + ". " + questionsArray[index].question +'</span>';
     let option_tag = '<div class="option"><span>'+ questionsArray[index].options[0] +'</span></div>'
     + '<div class="option"><span>'+ questionsArray[index].options[1] +'</span></div>'
     + '<div class="option"><span>'+ questionsArray[index].options[2] +'</span></div>'
     + '<div class="option"><span>'+ questionsArray[index].options[3] +'</span></div>';
-    que_text.innerHTML = que_tag; 
+    que_name.innerHTML = que_tag; 
     option_list.innerHTML = option_tag; 
     que_title.innerHTML = questionsArray[index].category; 
 
@@ -137,12 +132,9 @@ function showQuestions(index){
     }
 }
 
-let tickIcon = '<div class="icon tick"><i class="fas fa-check"></i></div>';
-let crossIcon = '<div class="icon cross"><i class="fas fa-times"></i></div>';
 
 function  optionSelected(answer){
     clearInterval(counter);
-    clearInterval(counterLine);
     let userAns = answer.textContent;
     let correctAns = questionsArray[que_count].answer;
     const allOptions = option_list.children.length;
@@ -152,16 +144,14 @@ function  optionSelected(answer){
         console.log(userScore);
         answer.classList.add("correct");
         console.log("Answer is correct");
-        answer.insertAdjacentHTML("beforeend", tickIcon);
+
     }else{
         answer.classList.add("incorrect");
          console.log("Answer is wrong");
-         answer.insertAdjacentHTML("beforeend", crossIcon);
 
          for (i = 0; i < allOptions; i++) {
         if(option_list.children[i].textContent == correctAns){
             option_list.children[i].setAttribute("class", "option correct")
-            option_list.children[i].insertAdjacentHTML("beforeend", tickIcon);
             }
         }
     }
@@ -176,28 +166,28 @@ function showResult(){
     console.log("total score " + totalScore);
     cardCount += 1;
     console.log("card count " + cardCount);
-    info_box.classList.remove("activeInfo"); 
+    rule_box.classList.remove("activeInfo"); 
     quiz_box.classList.remove("activeQuiz"); 
     result_box.classList.add("activeResult"); 
-    const scoreText = result_box.querySelector(".score_text");
+    const cardScore = result_box.querySelector(".card_score");
     if (userScore > 3){ 
         let scoreTag = '<span>and congrats! 🎉, You got <p>'+ userScore +'</p> out of <p>'+ questions1.length +'</p></span>';
-        scoreText.innerHTML = scoreTag;  
+        cardScore.innerHTML = scoreTag;  
     }
     else if(userScore > 1){ 
         let scoreTag = '<span>and nice 😎, You got <p>'+ userScore +'</p> out of <p>'+ questions1.length +'</p></span>';
-        scoreText.innerHTML = scoreTag;
+        cardScore.innerHTML = scoreTag;
     }
     else{ 
         let scoreTag = '<span>and sorry 😐, You got only <p>'+ userScore +'</p> out of <p>'+ questions1.length +'</p></span>';
-        scoreText.innerHTML = scoreTag;
+        cardScore.innerHTML = scoreTag;
     }
 }
 
 function showFinal(){ 
     result_box.classList.remove("activeResult"); 
     final_box.classList.add("activeFinal")
-    const finalText = final_box.querySelector(".final_text");
+    const finalText = final_box.querySelector(".final_score");
     if (userScore = 15){ 
         let finalTag = '<span>and congrats! 🎉, You got <p>'+ totalScore +'</p></span>';
         finalText.innerHTML = finalTag;  
@@ -229,20 +219,9 @@ function startTimer(time){
     }
 }
 
-function startTimerLine(time){
-    counterLine = setInterval(timer, 29);
-    function timer(){
-        time += 1;
-        timeLine.style.width = time + "px";
-        if(time > 549){
-            clearInterval(counterLine);
-        }    
-    }
-}
-
 function queCounter(index){
     //creating a new span tag and passing the question number and total question
-    const bottom_ques_counter = document.querySelector(".total_que");
-    let totalQueCountTag = '<span><p>'+ index +'</p> of <p>'+ questions1.length +'</p> Questions</span>';
-    bottom_ques_counter.innerHTML = totalQueCountTag;  //adding new span tag inside bottom_ques_counter
+    const question_counter = document.querySelector(".total");
+    let totalQuestionCountTag = '<span><p>'+ index +'</p> of <p>'+ questions1.length +'</p> Questions</span>';
+    question_counter.innerHTML = totalQuestionCountTag;  //adding new span tag inside bottom_counter
 }
